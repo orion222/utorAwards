f06cdebd4c15e0af2ba56663c6bd390d9b1d9196
Jeremy Chong <chong.jeremy@hotmail.com>
2025-11-15 02:03:58 -0500

    feat: add Inter font and basic styling components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import './styles/theme.css'
+import './styles/utils.css'
+import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
