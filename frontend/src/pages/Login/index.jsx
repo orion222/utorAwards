ba6b2245ef8fd4afa39ec964f8c5f78f159cf223
Jeremy Chong <chong.jeremy@hotmail.com>
2025-11-16 00:41:26 -0500

    fix spacing

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -12,25 +12,26 @@ function Login() {
     const [error, setError] = useState("");
     const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+    const handleLogin = async (e) => {
+        e.preventDefault();
 
-    try {
-      const { data: authData } = await api.post("/auth/tokens", {
-        utorid,
-        password,
-      });
+        try {
+            const { data: authData } = await api.post("/auth/tokens", {
+                utorid,
+                password,
+            });
 
-      const { data: userData } = await api.get("/users/me", {
-        headers: {
-          Authorization: `Bearer ${authData.token}`,
-        },
-      });
+            const { data: userData } = await api.get("/users/me", {
+                headers: {
+                Authorization: `Bearer ${authData.token}`,
+                },
+            });
 
-      login(authData.token, userData);
-      navigate("/dashboard");
-    } catch (error) {
-      console.warn(error.response?.data || error.message);
+            login(authData.token, userData);
+            navigate("/dashboard");
+        } catch (error) {
+            console.warn(error.response?.data || error.message);
+        }
     }
 
     return (
