2dba58e5804162962aa98b518b4b822bc4ffa860
Brady Wong <bradywong2k3@gmail.com>
2025-11-16 16:05:00 -0500

    dashboard progress

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useUser } from "../../context/UserContext";
 import api from "../../api/api";
 import { useNavigate } from "react-router-dom";
@@ -6,8 +6,29 @@ import './style.css'
 
 function Dashboard() {
     const { user } = useUser();
+    const { cookies } = useUser();
     const navigate = useNavigate();
+    const [transactions, setTransactions] = useState([]);
+    const [promotions, setPromotions] = useState([]);
+    const [events, setEvents] = useState([]);
 
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const { data: transactionData } = await api.get("/users/me/transactions", {
+                headers: {
+                  Authorization: `Bearer ${cookies.token}`,
+                },
+                params: {limit: 3}
+            });
+            setTransactions(transactionData.results);
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        }
+        fetchData();
+    }, [cookies.token]);
+   
     const viewWallet = () => {
         navigate("/wallet");
     };
@@ -26,32 +47,59 @@ function Dashboard() {
         e.preventDefault();
         navigate("/events");
     };
-
+    console.log(transactions);
     return <div>
         <div className="user-header">
             <p className="welcome-message">Welcome back, {user.name}!</p>
             <p className="point-summary">Here's your point summary</p>
             <p className="points">{user.points} points</p>
-            <button type="submit" onClick={viewWallet}>View My Wallet</button>
+            <button type="submit" className="view-wallet-button" onClick={viewWallet}>View My Wallet</button>
         </div>
         <div className="transactions-container">
             <div>
                 <p>Recent Transactions</p>
-                <div>[transactions go here]</div>
+                <div className="transaction-list">
+                    {transactions.length > 0 ? (
+                                transactions.map(t => (
+                                    <div key={t.id} className="transaction-card">
+                                        <div className="transaction-left">
+                                            <div className="tag-container">
+                                                <p>{t.type}</p>
+                                                <p>{t.processed ? "Processed" : "Unproccessed"}</p>
+                                            </div>
+                                            <p>Remark: {t.remark}</p>
+                                        </div>
+                                        <div className="transaction-right">
+                                            <p>{t.amount ? (t.amount) : (t.earned)}</p>
+                                        </div>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>No transactions found.</p>
+                            )}
+                </div>
                 <a href="/transactions" onClick={viewTransactions}>(View all transactions)</a>
             </div>
         </div>
         <div className="promotions-container">
             <div>
                 <p>Promotions For You</p>
-                <div>[promtions go here]</div>
+                <div className="promotion-list">
+                    <div className="promotion-card">[promotion 1 goes here]</div>
+                    <div className="promotion-card">[promotion 2 goes here]</div>
+                    <div className="promotion-card">[promotion 3 goes here]</div>
+                </div>
                 <a href="promotions" onClick={viewPromotions}>(View all promotions)</a>
             </div>
         </div>
         <div className="events-container">
             <div>
                 <p>Upcoming Events</p>
-                <div>[events go here]</div>
+                <div className="event-list">
+                    <div className="event-card">[promotion 1 goes here]</div>
+                    <div className="event-card">[promotion 2 goes here]</div>
+                    <div className="event-card">[promotion 3 goes here]</div>
+                </div>
                 <a href="events" onClick={viewEvents}>(View all events)</a>
             </div>
         </div>
