4fffda6c166c62c116a378f5b8188a1cc0f9f82d
Brady Wong <bradywong2k3@gmail.com>
2025-11-17 01:06:05 -0500

    promotion endedpoint fix

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,7 +7,6 @@ const {
   createRedemption,
   retrieveTransactions,
   retrieveEvents,
-  retrievePromotions,
   updateMyUserInfo,
   getMyUserInfo,
   updateMyPassword,
@@ -73,8 +72,6 @@ userRouter.post("/me/transactions", verifyToken, createRedemption);
 userRouter.get("/me/transactions", verifyToken, retrieveTransactions);
 
 userRouter.get("/me/events", verifyToken, retrieveEvents);
-
-userRouter.get("/me/promotions", verifyToken, retrievePromotions);
 
 userRouter.post("/:userId/transactions", verifyToken, createTransfer);
 
