b6c555f175ba54163f863065ffbbd938ac1d9ab8
kobec <kobe.chui@mail.utoronto.ca>
2025-11-22 23:40:52 -0500

    Completed create purchase; Made common component FormCard customizable in width and padding

--- frontend/src/components/common/FormCard.jsx
+++ frontend/src/components/common/FormCard.jsx
@@ -1,9 +1,11 @@
 import { useTheme, useMediaQuery } from "@mui/material";
 import { Box, Card, CardContent } from "@mui/material";
 
-function FormCard({ children }) {
+function FormCard({ width, contentPadding, children }) {
   const theme = useTheme();
   const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
+  const cardWidth = width || 420;
+  const padding = contentPadding || 4;
 
   return (
     <Box
@@ -15,9 +17,9 @@ function FormCard({ children }) {
       }}
     >
       {isSmall ? (
-        <Box sx={{ width: "100%", p: 4 }}>{children}</Box>
+        <Box sx={{ width: "100%", p: padding }}>{children}</Box>
       ) : (
-        <Card sx={{ width: 420, p: 4 }}>
+        <Card sx={{ width: cardWidth, p: padding }}>
           <CardContent>{children}</CardContent>
         </Card>
       )}
