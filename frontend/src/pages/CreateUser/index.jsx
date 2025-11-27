e99fe2c462bb2d1b94f5da55d6c41072eac65cb2
kobec <kobe.chui@mail.utoronto.ca>
2025-11-27 11:07:18 -0500

    Removed localStorage related features

--- frontend/src/pages/CreateUser/index.jsx
+++ frontend/src/pages/CreateUser/index.jsx
@@ -10,8 +10,6 @@ import useToast from "../../components/common/hooks/useToast.jsx";
 import LabeledField from "../../components/common/LabeledField.jsx";
 import ConfirmDialog from "../../components/common/ConfirmDialog.jsx";
 
-const APP_PREFIX = "utorAwards:";
-
 export default function CreateUser() {
     const { showToast, ToastComponent } = useToast();
     const [showConfirmDialog, setShowConfirmDialog] = useState(false);
@@ -35,7 +33,6 @@ export default function CreateUser() {
         }
         try {
             const response = await api.post(`/users`, payload);
-            localStorage.removeItem(`${APP_PREFIX}createUserForm`);
             reset(defaultValues);
             showToast("User profile created successfully", "success");
         }
@@ -62,33 +59,15 @@ export default function CreateUser() {
         email: "",
     };
 
-    const savedValues = (() => {
-        try {
-            const items = localStorage.getItem(`${APP_PREFIX}createUserForm`);
-            return items ? JSON.parse(items) : defaultValues;
-        }
-        catch {
-            return defaultValues;
-        }
-    })();
-
     const { control, handleSubmit, reset, formState: { errors, isSubmitting }, getValues, setValue } = useForm({
         resolver: yupResolver(schema),
         mode: "onChange",
         reValidateMode: "onChange",
-        defaultValues: {...savedValues}
+        defaultValues: defaultValues
     });
 
     const formValues = useWatch({ control });
     
-    useEffect(() => {
-        try {
-            localStorage.setItem(`${APP_PREFIX}createUserForm`, JSON.stringify(formValues || {}));
-        } catch (err) {
-            console.error("Failed to persist createUserForm to localStorage:", err); //debugging
-        }
-    }, [formValues]);
-
     return (
         <Container
             sx={{
