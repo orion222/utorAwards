e9ef6002420f9b46c12cad1bf540988a542fcd44
orion <orion@singlekey.com>
2025-11-27 01:54:16 -0500

    make edit event start tiem must be in the future

--- frontend/src/pages/Events/constants.js
+++ frontend/src/pages/Events/constants.js
@@ -3,11 +3,14 @@ import * as yup from "yup";
 export const editEventSchema = yup.object({
   name: yup.string().required("Event title is required"),
   description: yup.string().required("Description is required"),
-  startTime: yup.date().required("Start date is required"),
+  startTime: yup
+    .date()
+    .min(new Date(), "Start time must be in the future")
+    .required("Start time is required"),
   endTime: yup
     .date()
-    .min(yup.ref("startTime"), "End date cannot be before start date")
-    .required("End date is required"),
+    .min(yup.ref("startTime"), "End time cannot be before start time")
+    .required("End time is required"),
   location: yup.string().required("Location is required"),
   pointsRemain: yup
     .number()
