dd86d47fe7bbea5820077e7dec6630500e32b2cd
Brady Wong <bradywong2k3@gmail.com>
2025-11-27 20:28:49 -0500

    formatting

--- frontend/src/pages/PromotionDetails/index.jsx
+++ frontend/src/pages/PromotionDetails/index.jsx
@@ -1,15 +1,4 @@
-import { useState, useEffect } from "react";
-import { useUser } from "../../context/UserContext";
-import api from "../../api/api";
-import { useNavigate, useParams, useLocation } from "react-router-dom";
-import {Card, CardContent, Stack, Chip, Box, Typography, Accordion, AccordionSummary, useMediaQuery, useTheme, AccordionDetails, Divider, Button, Modal,} from "@mui/material";
-import theme from '../../theme.js';
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import LocationPinIcon from "@mui/icons-material/LocationPin";
-import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import RSVPSuccessModal from "../../components/common/RSVPSuccessModal.jsx";
-import UnRSVPSuccessModal from "../../components/common/UnRSVPSuccessModal.jsx";
+import { useParams, useLocation } from "react-router-dom";
 import PromotionCard from "../../components/common/PromotionCard";
 
 
@@ -17,7 +6,6 @@ function PromotionDetails() {
     const { promotionId } = useParams();
     const { state } = useLocation();
 
-    console.log(state);
     return (
         <PromotionCard promotion={state.promotion} key={promotionId} hover={false}></PromotionCard>
     )
--- frontend/src/pages/TransactionDetails/index.jsx
+++ frontend/src/pages/TransactionDetails/index.jsx
@@ -1,15 +1,4 @@
-import { useState, useEffect } from "react";
-import { useUser } from "../../context/UserContext";
-import api from "../../api/api";
-import { useNavigate, useParams, useLocation } from "react-router-dom";
-import {Card, CardContent, Stack, Chip, Box, Typography, Accordion, AccordionSummary, useMediaQuery, useTheme, AccordionDetails, Divider, Button, Modal,} from "@mui/material";
-import theme from '../../theme.js';
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import LocationPinIcon from "@mui/icons-material/LocationPin";
-import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import RSVPSuccessModal from "../../components/common/RSVPSuccessModal.jsx";
-import UnRSVPSuccessModal from "../../components/common/UnRSVPSuccessModal.jsx";
+import { useParams, useLocation } from "react-router-dom";
 import TransactionItemCard from "../../components/common/TransactionItemCard.jsx";
 
 
@@ -18,11 +7,7 @@ function TransactionDetails() {
     const { state } = useLocation();
 
     return (
-        <Box>
-            <TransactionItemCard transaction={state.transaction} key={transactionId} hover={false}>
-            
-            </TransactionItemCard>
-        </Box>
+        <TransactionItemCard transaction={state.transaction} key={transactionId} hover={false}></TransactionItemCard>
     )
 }
 
