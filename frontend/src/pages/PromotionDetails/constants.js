36cb36c6e311971f8db0ce116c718d1b80d0e315
Brady Wong <bradywong2k3@gmail.com>
2025-12-03 22:20:48 -0500

    fixed bug forcing rate and points on update to be non null

--- frontend/src/pages/PromotionDetails/constants.js
+++ frontend/src/pages/PromotionDetails/constants.js
@@ -13,13 +13,19 @@ export const editPromotionSchema = yup.object({
     .required("End time is required"),
   rate: yup
     .number()
+    .nullable()
+    .transform((value, original) => original === "" ? null : value)
     .typeError("Rate must be a number")
     .min(0, "Rate must be at least 0"),
   points: yup
     .number()
+    .nullable()
+    .transform((value, original) => original === "" ? null : value)
     .typeError("Points must be a number")
     .min(1, "Points must be at least 1"),
   minSpending: yup
     .number()
+    .nullable()
+    .transform((value, original) => original === "" ? null : value)
     .typeError("minimum spending must be a number")
 });
